d43f42eccae517cc018246e24fcb24ce0000d70d
jiang <jiang040330@gmail.com>
2020-05-03 10:55:20 +0800

    change fangxue

--- 20181201小明放学.cpp
+++ 20181201小明放学.cpp
@@ -8,17 +8,17 @@ long long sum=0;
 int main(){
 	scanf("%lld%lld%lld",&r,&y,&g);
 	scanf("%lld",&N);
-	long long flag[N],time[N],a[N];
+	long long flag,time,a;
 	for(int i=0;i<N;i++){
-		scanf("%lld%lld",&flag[i],&time[i]);
+		scanf("%lld%lld",&flag,&time);
 
-		if(flag[i]==1)a[i]=time[i]=r-time[i];
-		else if(flag[i]==2)a[i]=time[i]=r+y+g-time[i];
-		else if(flag[i]==3)a[i]=time[i]=r+g-time[i];
-		if(flag[i])time[i]=(a[i]+sum)%(r+y+g);
-		if(flag[i]==0)sum=sum+time[i];
-		else if(flag[i]&&(time[i]<r))sum=sum+r-time[i];
-		else if(flag[i]&&(time[i]>=r+g))sum=sum+r*2+y+g-time[i];
+		if(flag==1)a=time=r-time;
+		else if(flag==2)a=time=r+y+g-time;
+		else if(flag==3)a=time=r+g-time;
+		if(flag)time=(a+sum)%(r+y+g);
+		if(flag==0)sum=sum+time;
+		else if(flag&&(time<r))sum=sum+r-time;
+		else if(flag&&(time>=r+g))sum=sum+r*2+y+g-time;
 
 	}
 	printf("%lld",sum);
