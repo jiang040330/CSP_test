de3d3f8b9d3b25b32f98e7783f88e077988f17d8
jiang <jiang040330@gmail.com>
2020-05-03 10:47:47 +0800

    change shangxue

--- 20181201小明上学.cpp
+++ 20181201小明上学.cpp
@@ -1,21 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int r,y,g;
-int N;
-int sum=0;
-
 int main(){
+
+    int r,y,g;
+    int N;
+    int sum=0;
+
 	scanf("%d%d%d",&r,&y,&g);
 	scanf("%d",&N);
-	int flag[N],time[N];
+	int flag,time;
 	for(int i=0;i<N;i++){
-		scanf("%d%d",&flag[i],&time[i]);
-		if(flag[i]==0)sum=sum+time[i];
-		else if(flag[i]==1)sum=sum+time[i];
-		else if(flag[i]==2)sum=sum+time[i]+r;
-		
+		scanf("%d%d",&flag,&time);
+		if(flag==0)sum=sum+time;
+		else if(flag==1)sum=sum+time;
+		else if(flag==2)sum=sum+time+r;
+
 	}
 	printf("%d",sum);
-	return 0; 
+	return 0;
 }
